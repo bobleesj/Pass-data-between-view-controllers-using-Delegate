df2c5715106808a2f8380d20724fa1b8e973f0fd
Bob Lee <Bob@Bobs-MacBook-Pro.local>
2016-12-12 19:23:16 +0900

    I added a file for ya Dennis

--- PassData with Bob/FirstVC.swift
+++ PassData with Bob/FirstVC.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstVC: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
+
+class SecondVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+}
+
 
